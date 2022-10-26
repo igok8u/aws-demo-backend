aa1944a6583c223a43f50b0271eb95e5703f8a83
Ihar Kuzniatsou <ihar_kuzniatsou@epam.com>
2022-10-26 20:22:55 +0200

    Init project

---
+++ src/main/java/dev/igok8u/awsdemobackend/AwsDemoBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.igok8u.awsdemobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDemoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDemoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/igok8u/awsdemobackend/AwsDemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.igok8u.awsdemobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDemoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
